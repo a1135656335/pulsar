0075d77fecb3197d31396382f412ba77fc888d31
a1135656335 <1135656335@qq.com>
2021-08-26 17:08:36 +0800

    websocket权限系统集成
     xzzh内部权限系统集成

--- pulsar-websocket/src/test/java/org/apache/pulsar/websocket/WebSocketHttpServletRequestWrapperTest.java
+++ pulsar-websocket/src/test/java/org/apache/pulsar/websocket/WebSocketHttpServletRequestWrapperTest.java
@@ -26,13 +26,8 @@ import org.testng.annotations.Test;
 
 /** WebSocketHttpServletRequestWrapper test. */
 public class WebSocketHttpServletRequestWrapperTest {
-
   private static final String TOKEN =
-      "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ0ZXN0LXVzZXIifQ.U387jG"
-          + "-gmpEXNmTjbnnnk24jCXnfy7OTiQhhhOdXPgV2wEvZYr83KRSmH54wJQr4V2FCWIFb_6mBc_"
-          + "E2acpfpfBOTTzrtietfhd6wE5uOP2NXaLpy_kUDsE3ZQGKPEsn18cWQUw54GAzS1oRcG9TnoqSCSFFGabvo"
-          + "FTiOMHoBQ3ZHO3TqAGqlJlRF5ZXMkRtQ9vwbPC-mlwIfRrRIJfK5_ijPRkpgFSEvAwp0rX6roz08SyTj_"
-          + "d4UNT96nsEL6sRNTpZMQ0qNj2_LMKFnwF3O_xe43-Uen3TllkAzhNd9Z6qIxyJyFbaFyWAVgiAfoFWQD0v4EmV96ZzKZvv3CbGjw";
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiItMiIsImF1ZCI6InB1bHNhciIsImF1ZElkIjoiMCIsInJvbGVzIjoiYnJva2VyX2FkbWluLGZ1bmN0aW9uX2FkbWluLHByb3h5X2FkbWluLGFwcF9hZG1pbiIsImlzcyI6Inh6emguY29tIiwic3ViVHlwZSI6IjUiLCJleHAiOiIyMDMxLTA4LTE5IDIwOjIxOjEzIiwiaWF0IjoiMjAyMS0wOC0yNSAyMDo0NzozOSIsImp0aSI6IjI3MjgxNDQ0MDQ4MCJ9.a5NA_iDM4iBh02KaVzWVHRjzzMsBVIQVybvhykAUXZnu3EfT7lyHWAGRtUXH7EQHxE07QtGSrkwUecUDbMAc6rmhN_W0qG7neMuA0ZH7Wl3i6sx-Lz6IoyHxTwTu_J2DuF-GSqPVhoyjktHCGnlrAe6khvZHZO8MzTRU17gpsHXUHL0f8YcB54TLO3QKYtDjZDniAnikp3t9Q_XgSd584zWS3sU4SZfdJAL0T8rtyFvk2n3oc6XmtmDQ1ev7qh6XwbJMxAg3ckk5dQJg2SPlDyeqmqEgZLjUPIWMLd2stll7WPHPwAN_XGiG7AbycMtsJxxWFL_iT5MIoYC1lSzUUk6RxfSj6Zm-hfno043XzDZSkw5m5MV_oH8l9rSHQ5s_x4vdqBROEQSy65zZEJFuWYzK0B8Ur5tD12f8lAXBU8fk7GK3NInCU23O9YSCHLr9-86DPLSHc9bNtesw-F93BjnKwWEtZr-TxmiDWcoM-JPZF97yfpsim-Urp_Fe3lTNwBowOUmUmMNMrXaCcXZrRxurFOUau8nxXxpZcHzrQ6lEly4L4myEqJDocu8mX30h890NjumSkEHZSN2Ng5a7lc9p-OsO2Gi5V4nEv7bM2XLg8p7icXQyMDZjJ94S9joSzkyNlPg4-DzYxlEY_MWf2L-GuylNUpUrP49NPulOeGc";
   private static final String BEARER_TOKEN =
       WebSocketHttpServletRequestWrapper.HTTP_HEADER_VALUE_PREFIX + TOKEN;
 
@@ -93,11 +88,10 @@ public class WebSocketHttpServletRequestWrapperTest {
         .thenReturn(TOKEN);
     WebSocketHttpServletRequestWrapper webSocketHttpServletRequestWrapper =
         new WebSocketHttpServletRequestWrapper(httpServletRequest);
-
-    Assert.assertEquals(
-        "test-user",
+    String ret =
         service
             .getAuthenticationService()
-            .authenticateHttpRequest(webSocketHttpServletRequestWrapper));
+            .authenticateHttpRequest(webSocketHttpServletRequestWrapper);
+    Assert.assertEquals("test-user", ret);
   }
 }
